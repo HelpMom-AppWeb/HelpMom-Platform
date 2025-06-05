fb1fa9b89af765121c96a83fedd26f517b551cba
RominaMaita <u202213765@upc.edu.pe>
2025-06-05 11:01:00 -0500

    feat(health-monitoring): add health data controller.

---
+++ webexperts.helpmom.platform.API/HealthMonitoring/Interfaces/REST/HealthDataController.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using webexperts.helpmom.platform.API.HealthMonitoring.Domain.Services;
+
+
+namespace webexperts.helpmom.platform.API.HealthMonitoring.Interfaces.REST;
+
+
+[ApiController]
+[Route("api/v1/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+[Tags("Health Data")]
+
+public class HealthDataController(IHealthDataCommandService healthDataCommandService, IHealthDataQueryService healthDataQueryService) : ControllerBase
+{
+    
+    
+}
