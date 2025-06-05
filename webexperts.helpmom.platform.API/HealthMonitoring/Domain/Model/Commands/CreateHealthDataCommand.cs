1e0a7027a0ded8964ecb6c85288b6a95795ca8e3
RominaMaita <u202213765@upc.edu.pe>
2025-06-05 10:59:29 -0500

    feat(health-monitoring): add commands and queries.

---
+++ webexperts.helpmom.platform.API/HealthMonitoring/Domain/Model/Commands/CreateHealthDataCommand.cs
@@ -0,0 +1,9 @@
+﻿
+namespace webexperts.helpmom.platform.API.HealthMonitoring.Domain.Model.Commands;
+
+public record CreateHealthDataCommand(
+    int HeartRate,
+    decimal Temperature,
+    decimal Weight,
+    int OxygenSaturation,
+    int PatientId);
