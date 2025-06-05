97a78cf3a180796b1a774ac61214e6b31524dd06
RominaMaita <u202213765@upc.edu.pe>
2025-06-05 11:00:46 -0500

    feat(health-monitoring): add assembler components.

---
+++ webexperts.helpmom.platform.API/HealthMonitoring/Interfaces/REST/Transform/CreateHealthDataSourceCommandFromResourceAssembler.cs
@@ -0,0 +1,11 @@
+﻿using webexperts.helpmom.platform.API.HealthMonitoring.Domain.Model.Commands;
+using webexperts.helpmom.platform.API.HealthMonitoring.Interfaces.REST.Resources;
+
+namespace webexperts.helpmom.platform.API.HealthMonitoring.Interfaces.REST.Transform;
+
+public static class CreateHealthDataSourceCommandFromResourceAssembler
+{
+    public static CreateHealthDataCommand ToCommandFromResource(CreateHealthDataResource resource) =>
+        new CreateHealthDataCommand(resource.HeartRate, resource.Temperature, resource.Weight,
+            resource.OxygenSaturation, resource.PatientId);
+}
---
+++ webexperts.helpmom.platform.API/HealthMonitoring/Interfaces/REST/Transform/HealthDataResourceFromEntityAssembler.cs
@@ -0,0 +1,11 @@
+﻿using webexperts.helpmom.platform.API.HealthMonitoring.Domain.Model.Aggregates;
+using webexperts.helpmom.platform.API.HealthMonitoring.Interfaces.REST.Resources;
+
+namespace webexperts.helpmom.platform.API.HealthMonitoring.Interfaces.REST.Transform;
+
+public static class HealthDataResourceFromEntityAssembler
+{
+    public static HealthDataResource ToResourceFromEntity(HealthData entity) =>
+        new HealthDataResource(entity.Id, entity.HeartRate, entity.Temperature, entity.Weight, entity.OxygenSaturation,
+            entity.PatientId);
+}
