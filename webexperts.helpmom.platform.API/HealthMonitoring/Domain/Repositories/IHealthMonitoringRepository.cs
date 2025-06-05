54d41c395c880127f2716fd84b608f3ebed8c448
RominaMaita <u202213765@upc.edu.pe>
2025-06-05 11:00:20 -0500

    feat(health-monitoring): add repositories.

---
+++ webexperts.helpmom.platform.API/HealthMonitoring/Domain/Repositories/IHealthMonitoringRepository.cs
@@ -0,0 +1,11 @@
+﻿using webexperts.helpmom.platform.API.HealthMonitoring.Domain.Model.Aggregates;
+using webexperts.helpmom.platform.API.Shared.Domain.Repositories;
+
+namespace webexperts.helpmom.platform.API.HealthMonitoring.Domain.Repositories;
+
+public interface IHealthMonitoringRepository : IBaseRepository<Model.Aggregates.HealthData>
+{
+
+    Task<IEnumerable<HealthData>> FindByPatientIdAsync(int patientId);
+    
+}
