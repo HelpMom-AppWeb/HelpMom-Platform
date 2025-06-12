6af6dae9706539ffad73645b0fff7f52189c3597
Stephano <Stephanoescu@gmail.com>
2025-06-12 16:52:51 -0500

    feat: add Duration entity.

---
+++ webexperts.helpmom.platform/Domain/Model/Entities/Duration.cs
@@ -0,0 +1,11 @@
+﻿namespace webexperts.helpmom.platform.Domain.Model.Entities;
+    
+    public class Duration
+    {
+        public string Value { get; private set; }
+    
+        public Duration(string value = "")
+        {
+            Value = value;
+        }
+    }
