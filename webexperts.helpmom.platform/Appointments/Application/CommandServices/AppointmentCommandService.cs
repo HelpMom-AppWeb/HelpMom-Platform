a93553bba0a9570f202dc2e92ee13487fc925a15
Camila Reyes <u201921442@upc.edu.pe>
2025-06-16 12:53:57 -0500

    feat: add AppointmentCommandService

---
+++ webexperts.helpmom.platform/Appointments/Application/CommandServices/AppointmentCommandService.cs
@@ -0,0 +1,27 @@
+﻿using webexperts.helpmom.platform.Appointments.Application.Commands;
+
+public class AppointmentCommandService : IAppointmentCommandService
+{
+    private readonly IAppointmentRepository _repository;
+
+    public AppointmentCommandService(IAppointmentRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<Appointment> Handle(CreateAppointmentDataCommand command)
+    {
+        var appointment = Appointment.Create(
+            command.DoctorId,
+            command.DoctorName,
+            command.Specialty,
+            command.Date,
+            command.Time,
+            command.Description,
+            command.PatientId,
+            command.PatientName);
+
+        await _repository.AddAsync(appointment);
+        return appointment;
+    }
+}
