fb575f59d8efe97d9935b6e56b28ce8397eca183
Gabriel Mamani <u202220659@upc.edu.pe>
2025-06-16 18:25:47 -0500

    feat : Add Commands

---
+++ webexperts.helpmom.platform/IAM/Domain/Model/Commands/SignInCommand.cs
@@ -0,0 +1,3 @@
+namespace webexperts.helpmom.platform.IAM.Domain.Model.Commands;
+
+public record SignInCommand(string Username, string Password);
---
+++ webexperts.helpmom.platform/IAM/Domain/Model/Commands/SignUpCommand.cs
@@ -0,0 +1,3 @@
+namespace webexperts.helpmom.platform.IAM.Domain.Model.Commands;
+
+public record SignUpCommand(string Username, string Password);
