d1ace0cec05512fef54e103a4fbc0bfbdd05d39c
Gabriel Mamani <u202220659@upc.edu.pe>
2025-06-16 18:50:46 -0500

    feat : add queries

--- webexperts.helpmom.platform/IAM/Domain/Model/Queries/GetUserByUsernameQuery.cs
+++ webexperts.helpmom.platform/IAM/Domain/Model/Queries/GetUserByUsernameQuery.cs
@@ -1,3 +1,9 @@
+using webexperts.helpmom.platform.IAM.Domain.Model.Repository;
+using webexperts.helpmom.platform.Shared.Domain.Repositories;
 namespace webexperts.helpmom.platform.IAM.Domain.Model.Queries;
 
-public record GetUserByUsernameQuery(string Username);
+public interface IUserRepository : IBaseRepository<User>
+{
+    Task<User?> FindByUsernameAsync(string username);
+    bool ExistsByUsername(string username);
+}
