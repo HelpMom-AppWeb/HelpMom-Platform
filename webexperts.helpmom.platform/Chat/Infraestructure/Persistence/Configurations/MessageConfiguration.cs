91f7286199dd926fbdcaeef53ce1e4ddf63deb72
Gabriel Mamani <u202220659@upc.edu.pe>
2025-06-16 17:44:23 -0500

    add infraestrucutre

---
+++ webexperts.helpmom.platform/Chat/Infraestructure/Persistence/Configurations/MessageConfiguration.cs
@@ -0,0 +1,29 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using webexperts.helpmom.platform.Chat.Domain.Model.Aggregates;
+using webexperts.helpmom.platform.Chat.Domain.Model.ValueObjects;
+
+namespace webexperts.helpmom.platform.Chat.Infrastructure.Persistence.Configurations
+{
+    public class MessageConfiguration : IEntityTypeConfiguration<Message>
+    {
+        public void Configure(EntityTypeBuilder<Message> builder)
+        {
+            builder.ToTable("Messages");
+
+            builder.HasKey(m => m.Id);
+
+            builder.OwnsOne(m => m.From, from =>
+            {
+                from.Property(f => f.UserId).HasColumnName("FromUserId");
+                from.Property(f => f.Role).HasColumnName("FromRole");
+            });
+
+            builder.Property(m => m.Text).IsRequired();
+            builder.Property(m => m.Timestramp);
+            builder.Property(m => m.patientId);
+        }
+    }
+}
+
