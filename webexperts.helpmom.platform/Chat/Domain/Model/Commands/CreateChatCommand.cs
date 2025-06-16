eb8350815e8c8121c25cb9f9c0f605fefd7b5815
gabriel <u202220659@upc.edu.pe>
2025-06-15 23:45:12 -0500

    feat : add CreateMessageCommand

---
+++ webexperts.helpmom.platform/Chat/Domain/Model/Commands/CreateChatCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace webexperts.helpmom.platform.Chat.Domain.Model.Commands;
+
+public record CreateMessageCommand(
+    int UserId,
+    string Role,
+    string Text,
+    long Timestamp,
+    string PatientId
+);
