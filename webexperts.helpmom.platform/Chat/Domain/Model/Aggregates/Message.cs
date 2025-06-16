94a23404a88fbc57636e5bf4445bb3dbe59b747b
Gabriel Mamani <u202220659@upc.edu.pe>
2025-06-16 17:42:57 -0500

    add message aggregate

--- webexperts.helpmom.platform/Chat/Domain/Model/Aggregates/Message.cs
+++ webexperts.helpmom.platform/Chat/Domain/Model/Aggregates/Message.cs
@@ -1,30 +1,24 @@
-﻿// Aggregates/Message.cs
-using webexperts.helpmom.platform.Chat.Domain.Model.ValueObjects;
+﻿using webexperts.helpmom.platform.Chat.Domain.Model.ValueObjects;
 
 namespace webexperts.helpmom.platform.Chat.Domain.Model.Aggregates
 {
     public class Message
     {
-        public string Text { get; private set; }
-        public long Timestamp { get; private set; } // ← DEBE ser long
+        public int Id { get; private set; }
         public From From { get; private set; }
-        public int PatientId { get; private set; }
+        public string Text { get; private set; }
+        public long Timestramp { get; private set; }
+        public int patientId { get; private set; }
 
-        public Message(string text, long timestamp, From from, int patientId)
+        public Message(string text, long timestramp, From from, int patientId)
         {
             Text = text;
-            Timestamp = timestamp;
+            Timestramp = timestramp;
             From = from;
-            PatientId = patientId;
+            this.patientId = patientId;
         }
 
-
-
-        private Message()
-        {
-        }
+        // Constructor para EF
+        private Message() { }
     }
 }
-
-      
-
