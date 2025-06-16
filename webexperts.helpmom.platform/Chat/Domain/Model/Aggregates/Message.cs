76e31902d35424e3f268302c794f280c49c0bcc0
gabriel <u202220659@upc.edu.pe>
2025-06-15 23:37:12 -0500

    feat: add message agreggate

---
+++ webexperts.helpmom.platform/Chat/Domain/Model/Aggregates/Message.cs
@@ -0,0 +1,28 @@
+﻿// Aggregates/Message.cs
+using webexperts.helpmom.platform.Chat.Domain.Model.ValueObjects;
+
+namespace webexperts.helpmom.platform.Chat.Domain.Model.Aggregates
+{
+    public class Message
+    {
+        public int Id { get; private set; }
+
+        public From From { get; private set; }
+
+        public string Text { get; private set; }
+
+        public long Timestamp { get; private set; }
+
+        public string PatientId { get; private set; }
+
+        public Message(From from, string text, long timestamp, string patientId)
+        {
+            From = from ?? throw new ArgumentNullException(nameof(from));
+            Text = text ?? throw new ArgumentNullException(nameof(text));
+            Timestamp = timestamp;
+            PatientId = patientId ?? throw new ArgumentNullException(nameof(patientId));
+        }
+
+        private Message() { }
+    }
+}
