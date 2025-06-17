50f054aebcff1cc1a4b13abfc8a262eb8ed57868
Stephano <Stephanoescu@gmail.com>
2025-06-16 23:12:47 -0500

    feat: add IMedicationRepository.

---
+++ webexperts.helpmom.platform.API/Domain/Repositories/IMedicationRepository.cs
@@ -0,0 +1,10 @@
+﻿using webexperts.helpmom.platform.API.Domain.Model.Entities;
+
+namespace webexperts.helpmom.platform.API.Domain.Repositories;
+
+public interface IMedicationRepository
+{
+    Task<IEnumerable<Medication>> FindByPrescriptionIdAsync(Guid prescriptionId);
+    Task<Medication?> FindByNameAndPrescriptionIdAsync(string name, Guid prescriptionId);
+    Task<bool> ExistsInPrescriptionAsync(string name, Guid prescriptionId);
+}
