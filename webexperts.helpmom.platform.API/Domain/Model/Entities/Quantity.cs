c4760bfe48f08095a70920032c2635ebd434e4b4
Stephano <Stephanoescu@gmail.com>
2025-06-16 23:26:58 -0500

    feat: add Quantity entity with validation for non-negative values.

---
+++ webexperts.helpmom.platform.API/Domain/Model/Entities/Quantity.cs
@@ -0,0 +1,12 @@
+﻿namespace webexperts.helpmom.platform.API.Domain.Model.Entities;
+
+public class Quantity
+{
+    public int Value { get; private set; }
+
+    public Quantity(int value = 0)
+    {
+        if (value < 0) throw new ArgumentException("La cantidad no puede ser negativa.");
+        Value = value;
+    }
+}
