a8dd24739df5fa2605d8de6d198b14e46bf44877
Stephano <Stephanoescu@gmail.com>
2025-06-17 09:38:11 -0500

    feat: add AppDbContext and repository pattern implementation.

---
+++ webexperts.helpmom.platform.API/Shared/Infrastructure/Persistence/EFC/Repositories/UnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using webexperts.helpmom.platform.API.Shared.Domain.Repositories;
+using webexperts.helpmom.platform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+namespace webexperts.helpmom.platform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+public class UnitOfWork(AppDbContext context) : IUnitOfWork
+{
+    /// <inheritdoc />
+    public async Task CompleteAsync()
+    {
+        await context.SaveChangesAsync();
+    }
+}
