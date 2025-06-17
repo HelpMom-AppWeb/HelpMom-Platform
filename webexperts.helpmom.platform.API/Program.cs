f559cd3f217a35b23f5d37ae7c21c87d799d6ff3
RominaMaita <u202213765@upc.edu.pe>
2025-06-17 17:34:44 -0500

    feat(health-monitoring): configure HealthData entity and add services to DI container

--- webexperts.helpmom.platform.API/Program.cs
+++ webexperts.helpmom.platform.API/Program.cs
@@ -1,4 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using webexperts.helpmom.platform.API.HealthMonitoring.Application.Internal.CommandServices;
+using webexperts.helpmom.platform.API.HealthMonitoring.Application.Internal.QueryServices;
+using webexperts.helpmom.platform.API.HealthMonitoring.Domain.Repositories;
+using webexperts.helpmom.platform.API.HealthMonitoring.Domain.Services;
+using webexperts.helpmom.platform.API.HealthMonitoring.Infrastructure.Persistence.EFC.Repositories;
 using webexperts.helpmom.platform.API.Shared.Domain.Repositories;
 using webexperts.helpmom.platform.API.Shared.Infrastructure.Interfaces.ASP.Configuration;
 using webexperts.helpmom.platform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
@@ -42,6 +47,12 @@ builder.Services.AddSwaggerGen(options => { options.EnableAnnotations(); });
 
 // Shared Bounded Context
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// Health Monitoring Bounded Context
+builder.Services.AddScoped<IHealthDataCommandService, HealthDataCommandService>();
+builder.Services.AddScoped<IHealthDataQueryService, HealthDataQueryService>();
+builder.Services.AddScoped<IHealthMonitoringRepository, HealthDataRepository>();
+
 
 var app = builder.Build();
 
--- webexperts.helpmom.platform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ webexperts.helpmom.platform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
+using webexperts.helpmom.platform.API.HealthMonitoring.Domain.Model.Aggregates;
 using webexperts.helpmom.platform.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
 
 namespace webexperts.helpmom.platform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
@@ -16,6 +17,14 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
+        
+        builder.Entity<HealthData>().HasKey(f => f.Id);
+        builder.Entity<HealthData>().Property(f => f.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<HealthData>().Property(f => f.HeartRate).IsRequired();
+        builder.Entity<HealthData>().Property(f => f.Temperature).IsRequired();
+        builder.Entity<HealthData>().Property(f => f.Weight).IsRequired();
+        builder.Entity<HealthData>().Property(f => f.OxygenSaturation).IsRequired();
+        builder.Entity<HealthData>().Property(f => f.PatientId).IsRequired();
         
         builder.UseSnakeCaseNamingConvention();
     }
