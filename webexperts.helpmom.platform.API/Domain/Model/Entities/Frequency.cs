aa8d28e3ab509f6c922addfb62c933e8e47960d2
Stephano <Stephanoescu@gmail.com>
2025-06-16 23:25:58 -0500

    feat: add Frequency value object.

---
+++ webexperts.helpmom.platform.API/Domain/Model/Entities/Frequency.cs
@@ -0,0 +1,11 @@
+﻿namespace webexperts.helpmom.platform.API.Domain.Model.Entities;
+    
+    public class Frequency
+    {
+        public string Value { get; private set; }
+    
+        public Frequency(string value = "")
+        {
+            Value = value;
+        }
+    }
