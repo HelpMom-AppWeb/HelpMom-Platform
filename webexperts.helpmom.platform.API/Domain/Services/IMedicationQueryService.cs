c59bd80d5f472d3307cd2408a1a8e15c4d5a5bbd
Stephano <Stephanoescu@gmail.com>
2025-06-16 23:12:41 -0500

    feat: add IMedicationQueryService.

---
+++ webexperts.helpmom.platform.API/Domain/Services/IMedicationQueryService.cs
@@ -0,0 +1,12 @@
+﻿using webexperts.helpmom.platform.API.Domain.Model.Entities;
+using webexperts.helpmom.platform.API.Domain.Model.Queries;
+
+namespace webexperts.helpmom.platform.API.Domain.Services;
+
+public interface IMedicationQueryService
+{
+    Task<Medication?> Handle(GetMedicationByIdQuery query);
+    Task<IEnumerable<Medication>> Handle(GetAllMedicationsQuery query);
+    Task<IEnumerable<Medication>> Handle(GetMedicationsByPrescriptionIdQuery query);
+    Task<bool> Handle(CheckMedicationExistsInPrescriptionQuery query);
+}
