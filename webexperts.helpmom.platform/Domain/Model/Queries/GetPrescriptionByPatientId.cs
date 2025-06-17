5f11541765070aa5cbe19d2f603f346a49a5d6b0
Stephano <Stephanoescu@gmail.com>
2025-06-16 23:15:14 -0500

    feat: add GetPrescriptionByPatientId.

--- webexperts.helpmom.platform/Domain/Model/Queries/GetPrescriptionByPatientId.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace webexperts.helpmom.platform.Domain.Model.Queries;
-
-public record GetPrescriptionByPatientIdQuery(int PatientId);
-
-public record GetActivePrescriptionsByPatientIdQuery(int PatientId);
-
