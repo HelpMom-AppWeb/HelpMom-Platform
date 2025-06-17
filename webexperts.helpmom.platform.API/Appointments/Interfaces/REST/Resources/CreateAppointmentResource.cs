f9518d0ffd0c8268ffdd3f739ae5b55007360557
Camila Reyes <u201921442@upc.edu.pe>
2025-06-17 02:30:22 -0500

    feat: add appointment controller and REST resources

---
+++ webexperts.helpmom.platform.API/Appointments/Interfaces/REST/Resources/CreateAppointmentResource.cs
@@ -0,0 +1,3 @@
+﻿namespace webexperts.helpmom.platform.API.Appointments.Interfaces.REST.Resources;
+
+public record CreateAppointmentResource(int DoctorId, string DoctorName, DateTime Date, TimeSpan Time, string Description, int PatientId, string PatientName);
---
+++ webexperts.helpmom.platform.API/Appointments/Interfaces/REST/Transform/AppointmentResourceFromEntityAssembler.cs
@@ -0,0 +1,12 @@
+﻿using webexperts.helpmom.platform.API.Appointments.Application.Internal.QueryServices;
+using webexperts.helpmom.platform.API.Appointments.Domain.Model.Aggregates;
+using webexperts.helpmom.platform.API.Appointments.Interfaces.REST.Resources;
+
+namespace webexperts.helpmom.platform.API.Appointments.Interfaces.REST.Transform;
+
+public static class AppointmentResourceFromEntityAssembler
+{
+    public static AppointmentResource ToResourceFromEntity(Appointment entity)=>
+        new AppointmentResource(entity.DoctorId, entity.DoctorName, entity.Date, entity.Time, entity.Description,
+            entity.PatientId, entity.PatientName);
+}
