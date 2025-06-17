e47a6bf71db27d8ff2c515d4db5b7cb1ab08b659
Camila Reyes <u201921442@upc.edu.pe>
2025-06-17 02:32:16 -0500

    feat: add appointment REST transformers and assemblers

---
+++ webexperts.helpmom.platform.API/Appointments/Interfaces/REST/Transform/CreateAppointmentSourceCommandFromResourceAssembler.cs
@@ -0,0 +1,12 @@
+﻿using webexperts.helpmom.platform.API.Appointments.Application.Internal.QueryServices;
+using webexperts.helpmom.platform.API.Appointments.Domain.Model.Commands;
+using webexperts.helpmom.platform.API.Appointments.Interfaces.REST.Resources;
+
+namespace webexperts.helpmom.platform.API.Appointments.Interfaces.REST.Transform;
+
+public class CreateAppointmentSourceCommandFromResourceAssembler
+{
+    public static CreateAppointmentCommand ToCommandFromResource(CreateAppointmentResource resource)=>
+        new CreateAppointmentCommand(resource.DoctorId, resource.DoctorName, resource.Date, resource.Time, resource.Description,
+            resource.PatientId, resource.PatientName);
+}
