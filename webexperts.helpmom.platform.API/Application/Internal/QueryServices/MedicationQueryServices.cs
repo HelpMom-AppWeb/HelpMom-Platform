8648141ced9c440811fddc9e387c3210f1adec29
Stephano <Stephanoescu@gmail.com>
2025-06-17 08:51:58 -0500

    feat: add MedicationQueryServices and Program.

---
+++ webexperts.helpmom.platform.API/Application/Internal/QueryServices/MedicationQueryServices.cs
@@ -0,0 +1,9 @@
+﻿using webexperts.helpmom.platform.API.Domain.Repositories;
+using webexperts.helpmom.platform.API.Domain.Services;
+
+namespace webexperts.helpmom.platform.API.Application.Internal.QueryServices;
+
+public class MedicationQueryServices (IMedicationRepository medicationRepository) : IMedicationQueryServices
+{
+    
+}
