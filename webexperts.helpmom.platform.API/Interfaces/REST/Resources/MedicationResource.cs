d944c33c2349664d3042dfc85111b503199740d2
Stephano <Stephanoescu@gmail.com>
2025-06-16 23:13:18 -0500

    feat: add MedicationResource.

---
+++ webexperts.helpmom.platform.API/Interfaces/REST/Resources/MedicationResource.cs
@@ -0,0 +1,23 @@
+﻿namespace webexperts.helpmom.platform.API.Interfaces.REST.Resources;
+
+public class MedicationResource
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Presentation { get; set; }
+    public string Manufacturer { get; set; }
+    public Guid PrescriptionId { get; set; }
+
+    // Constructor vacío
+    public MedicationResource() { }
+
+    // Constructor parametrizado
+    public MedicationResource(Guid id, string name, string presentation, string manufacturer, Guid prescriptionId)
+    {
+        Id = id;
+        Name = name;
+        Presentation = presentation;
+        Manufacturer = manufacturer;
+        PrescriptionId = prescriptionId;
+    }
+}
