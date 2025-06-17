ec3aad834b515e2e2e94ce25aa4b4e04c0542388
Stephano <Stephanoescu@gmail.com>
2025-06-16 23:25:34 -0500

    feat: add CreateMedication.

---
+++ webexperts.helpmom.platform.API/Domain/Model/Commands/CreateMedication.cs
@@ -0,0 +1,17 @@
+﻿namespace webexperts.helpmom.platform.API.Domain.Model.Commands;
+    
+    public class CreateMedicationCommand
+    {
+        public string Name { get; }
+        public string Presentation { get; }
+        public string Manufacturer { get; }
+        public Guid PrescriptionId { get; }
+    
+        public CreateMedicationCommand(string name, string presentation, string manufacturer, Guid prescriptionId)
+        {
+            Name = name;
+            Presentation = presentation;
+            Manufacturer = manufacturer;
+            PrescriptionId = prescriptionId;
+        }
+    }
