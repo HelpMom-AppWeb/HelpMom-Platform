4dbe5de4122566e64329f12a922a84a626dc7600
Stephano <Stephanoescu@gmail.com>
2025-06-16 23:14:36 -0500

    feat: add Frequency.

--- webexperts.helpmom.platform/Domain/Model/ValueObjects/Frequency.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace webexperts.helpmom.platform.Domain.Model.ValueObjects;
-
-public record Frequency(int TimesPerDay, string Schedule)
-{
-    public Frequency() : this(1, "Once daily") { }
-    
-    public override string ToString() => $"{TimesPerDay}x - {Schedule}";
-}
