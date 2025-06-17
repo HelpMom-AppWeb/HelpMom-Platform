011f43b8e86ee5df65a22c3fa7188148312e8aee
guestwhoo <u202223279@upc.edu.pe>
2025-06-16 19:07:23 -0500

    feat: added application layer

---
+++ webexperts.helpmom.platform.API/PatientManagement/Application/Internal/CommandServices/PatientCommandService.cs
@@ -0,0 +1,19 @@
+using webexperts.helpmom.platform.API.PatientManagement.Domain.Model.Aggregates;
+using webexperts.helpmom.platform.API.PatientManagement.Domain.Model.Commands;
+using webexperts.helpmom.platform.API.PatientManagement.Domain.Repositories;
+using webexperts.helpmom.platform.API.PatientManagement.Domain.Services;
+using webexperts.helpmom.platform.API.Shared.Domain.Repositories;
+
+namespace webexperts.helpmom.platform.API.PatientManagement.Application.Internal.CommandServices;
+
+public class PatientCommandService(IPatientRepository patientRepository, IUnitOfWork unitOfWork)
+    : IPatientCommandService
+{
+    public async Task<Patient> Handle(CreatePatientCommand command)
+    {
+        var patient = new Patient(command);
+        await patientRepository.AddAsync(patient);
+        await unitOfWork.CompleteAsync();
+        return patient;
+    }
+}
---
+++ webexperts.helpmom.platform.API/PatientManagement/Application/Internal/QueryServices/DoctorQueryService.cs
@@ -0,0 +1,6 @@
+namespace webexperts.helpmom.platform.API.PatientManagement.Application.Internal.QueryServices;
+
+public class DoctorQueryService
+{
+    
+}
