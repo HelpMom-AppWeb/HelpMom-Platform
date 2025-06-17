c45a0013ad9a0fe76bbd9f2f564b7836f6847614
Stephano <Stephanoescu@gmail.com>
2025-06-16 23:14:11 -0500

    feat: add Duration.

--- webexperts.helpmom.platform/Domain/Model/ValueObjects/Duration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace webexperts.helpmom.platform.Domain.Model.ValueObjects;
-
-public record Duration(int Value, string Unit)
-{
-    public Duration() : this(0, "days") { }
-    
-    public override string ToString() => $"{Value} {Unit}";
-}
