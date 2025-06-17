bf25775d487a6749963e8a3941e1c8ff5712dcf8
Stephano <Stephanoescu@gmail.com>
2025-06-16 23:16:05 -0500

    feat: add Quantity.

--- webexperts.helpmom.platform/Domain/Model/ValueObjects/Quantity.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace webexperts.helpmom.platform.Domain.Model.ValueObjects;
-
-public record Quantity(decimal Amount, string Unit)
-{
-    public Quantity() : this(0, "mg") { }
-    
-    public override string ToString() => $"{Amount} {Unit}";
-}
