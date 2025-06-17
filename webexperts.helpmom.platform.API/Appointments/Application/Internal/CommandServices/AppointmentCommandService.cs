30f30eb84052debb601ab8c419d49c7569997cb4
Camila Reyes <u201921442@upc.edu.pe>
2025-06-17 02:29:05 -0500

    feat: add appointment command services and application logic

---
+++ webexperts.helpmom.platform.API/Appointments/Application/Internal/CommandServices/AppointmentCommandService.cs
@@ -0,0 +1,27 @@
+﻿using webexperts.helpmom.platform.API.Appointments.Domain.Model.Aggregates;
+using webexperts.helpmom.platform.API.Appointments.Domain.Model.Commands;
+using webexperts.helpmom.platform.API.Appointments.Domain.Repositories;
+using webexperts.helpmom.platform.API.Appointments.Domain.Services;
+using webexperts.helpmom.platform.API.Shared.Domain.Repositories;
+
+namespace webexperts.helpmom.platform.API.Appointments.Application.Internal.CommandServices;
+
+public class AppointmentCommandService(IAppointmentRepository appointmentRepository, IUnitOfWork unitOfWork)
+    : IAppointmentCommandService
+{
+    public async Task<Appointment?> Handle(CreateAppointmentCommand command)
+    {
+        var appointment = new Appointment(
+            command.DoctorId,
+            command.DoctorName,
+            command.Date,
+            command.Time,
+            command.Description,
+            command.PatientId,
+            command.PatientName
+        );
+        await appointmentRepository.AddAsync(appointment);
+        await unitOfWork.CompleteAsync();
+        return appointment;
+    }
+}
