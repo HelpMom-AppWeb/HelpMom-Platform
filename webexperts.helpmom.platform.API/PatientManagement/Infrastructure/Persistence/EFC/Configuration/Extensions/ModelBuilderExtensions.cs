97c3b74a4556a7b1bc224a18f873508c906b0e03
GabrielRiveraOfficial <gabriverlay@gmail.com>
2025-06-18 17:56:41 -0500

    fix: fixed AppDbContext for the context

--- webexperts.helpmom.platform.API/PatientManagement/Infrastructure/Persistence/EFC/Configuration/Extensions/ModelBuilderExtensions.cs
+++ webexperts.helpmom.platform.API/PatientManagement/Infrastructure/Persistence/EFC/Configuration/Extensions/ModelBuilderExtensions.cs
@@ -11,30 +11,18 @@ public static class ModelBuilderExtensions
         // Patient Management Context
         builder.Entity<Doctor>().HasKey(d => d.Id);
         builder.Entity<Doctor>().Property(d => d.Id).IsRequired().ValueGeneratedOnAdd();
-        builder.Entity<Doctor>().Property(d => d.ProfileId).IsRequired();
-        builder.Entity<Doctor>()
-            .HasMany(d => d.Patients)
-            .WithOne(p => p.AssignedDoctor)
-            .HasForeignKey(p => p.AssignedDoctorId)
-            .IsRequired();
+        builder.Entity<Doctor>().Property(d => d.ProfileId);
         
         builder.Entity<Patient>().HasKey(p => p.Id);
         builder.Entity<Patient>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
-        builder.Entity<Patient>().Property(p => p.ProfileId).IsRequired();
+        builder.Entity<Patient>().Property(p => p.ProfileId);
         builder.Entity<Patient>().Property(p => p.Phone).HasMaxLength(15);
-        builder.Entity<Patient>().OwnsOne(p => p.Baby, b =>
-        {
-            b.WithOwner().HasForeignKey("MotherId");
-            b.Property(p => p.Id).HasColumnName("BabyId");
-        });
-        builder.Entity<Patient>().Property(p => p.AssignedDoctorId).IsRequired();
+        builder.Entity<Patient>().Property(p => p.AssignedDoctorId);
 
-        builder.Entity<Baby>().HasKey(p => p.Id);
-        builder.Entity<Baby>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
-        builder.Entity<Baby>().Property(p => p.Name).IsRequired().HasMaxLength(50);
-        builder.Entity<Baby>().Property(p => p.DateOfBirth).IsRequired();
-        builder.Entity<Baby>().Property(p => p.Gender).IsRequired().HasMaxLength(10);
-        builder.Entity<Baby>().Property(p => p.MotherId).IsRequired();
-
+        builder.Entity<Baby>().HasKey(b => b.Id);
+        builder.Entity<Baby>().Property(b => b.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<Baby>().Property(b => b.Name).HasMaxLength(50);
+        builder.Entity<Baby>().Property(b => b.DateOfBirth);
+        builder.Entity<Baby>().Property(b => b.Gender).HasMaxLength(10);
     }
 }
