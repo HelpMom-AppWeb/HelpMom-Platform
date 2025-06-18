861a0f58f0896d52920044104377637cc5ff6848
GabrielRiveraOfficial <gabriverlay@gmail.com>
2025-06-18 17:54:31 -0500

    fix: fixed Commands and Queries

--- webexperts.helpmom.platform.API/PatientManagement/Domain/Model/Commands/CreateBabyCommand.cs
+++
@@ -1,3 +0,0 @@
-namespace webexperts.helpmom.platform.API.PatientManagement.Domain.Model.Commands;
-
-public record CreateBabyCommand(string Name, DateTime DateOfBirth, string Gender, int MotherId);
--- webexperts.helpmom.platform.API/PatientManagement/Domain/Model/Commands/CreatePatientCommand.cs
+++ webexperts.helpmom.platform.API/PatientManagement/Domain/Model/Commands/CreatePatientCommand.cs
@@ -1,3 +1,3 @@
 namespace webexperts.helpmom.platform.API.PatientManagement.Domain.Model.Commands;
 
-public record CreatePatientCommand(int ProfileId, string Phone, int BabyId, int AssignedDoctorId);
+public record CreatePatientCommand(int ProfileId, string Phone, int AssignedDoctorId, string BabyName, DateTime BabyDateOfBirth, string BabyGender);
