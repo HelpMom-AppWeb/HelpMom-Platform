207f512c895b9bc55de062b091f34cbead0791c4
RominaMaita <u202213765@upc.edu.pe>
2025-07-03 22:43:45 -0500

    feat(health-monitoring): add UpdateHealthDataCommandFromResourceAssembler for command conversion

---
+++ webexperts.helpmom.platform.API/HealthMonitoring/Interfaces/REST/Transform/UpdateHealthDataCommandFromResourceAssembler.cs
@@ -0,0 +1,15 @@
+﻿using webexperts.helpmom.platform.API.HealthMonitoring.Domain.Model.Commands;
+using webexperts.helpmom.platform.API.HealthMonitoring.Interfaces.REST.Resources;
+
+namespace webexperts.helpmom.platform.API.HealthMonitoring.Interfaces.REST.Transform;
+
+public class UpdateHealthDataCommandFromResourceAssembler
+{
+    public static UpdateHealthDataCommand ToCommand(UpdateHealthDataResource resource) =>
+        new UpdateHealthDataCommand(
+            resource.HeartRate,
+            resource.Temperature,
+            resource.Weight,
+            resource.OxygenSaturation
+        );
+}
