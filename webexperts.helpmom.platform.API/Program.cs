e68a827cee507f2edd1d032646a7a9fea726a7aa
Gabriel Rivera <gabriverlay@gmail.com>
2025-07-06 19:59:25 -0500

    fix: update program.cs to allow swagger access on production environment

--- webexperts.helpmom.platform.API/Program.cs
+++ webexperts.helpmom.platform.API/Program.cs
@@ -109,11 +109,8 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Apply CORS Policy
 app.UseCors("AllowAllPolicy");
